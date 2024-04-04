87ac275a920a18f48ab1725359d0e91e9521bcf6
electrxety <er191712@gmail.com>
2024-04-04 19:40:57 -0400

    solved question 6, modifying if else statement to include a guard clause

--- src/modify.js
+++ src/modify.js
@@ -1,12 +1,29 @@
 const wildlyBiasedReview = (location) => {
-  if (location === 'NYC') {
+  if (location !== 'NYC') { //guard clause checks if a condition is not met first; if so, it will break out of the function instead of wasting computational resources running code just to see that the condition is not met
+    return console.log('Yea that place is cool I guess'); //console.log needed to maintain the functionality of original function
+} else if (location === 'NYC') {
     console.log('THE GREATEST CITY IN THE WORLD');
     console.log('THE CITY THAT NEVER SLEEPS');
     console.log('WOW WHAT A CITY');
-  } else {
-    console.log('Yea that place is cool I guess');
+    return
   }
 };
+
+// //first try: incorrect
+// const wildlyBiasedReview = (location) => {
+//   if (location !== 'NYC') return 'Yea that place is cool I guess';
+//   if (location === 'NYC') return 'THE GREATEST CITY IN THE WORLD';
+
+// //original code
+// const wildlyBiasedReview = (location) => {
+//   if (location === 'NYC') {
+//     console.log('THE GREATEST CITY IN THE WORLD');
+//     console.log('THE CITY THAT NEVER SLEEPS');
+//     console.log('WOW WHAT A CITY');
+//   } else {
+//     console.log('Yea that place is cool I guess');
+//   }
+// };
 
 module.exports = {
   wildlyBiasedReview,
