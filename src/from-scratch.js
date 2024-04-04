1a55a70c4b0a99eb1d6b7bbbce2375957b00e7eb
electrxety <er191712@gmail.com>
2024-04-04 17:02:12 -0400

    solved question 2

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -26,8 +26,38 @@ console.log(measureRain(0)); // "drought"
 console.log(measureRain(3)); // "average"
 console.log(measureRain(4)); // "rainy"
 
-const happyBirthdayPet = () => {
+/*Question 2: a function that takes two arguments, a string, breed and a number, age; should log a message in the following situations:
+  'snake', any age - 'Hiss hiss!'
+  'cat', less than 5 - 'Mew mew!'
+  'cat', 5 or more - 'Meow meow!'
+  'dog', less than 5 - 'Arf arf!'
+  'dog', 5 to less than 10 - 'Woof woof!'
+  'dog', 10 or more - 'Boof!' By default, just log a message of 'Happy birthday!'
+*/
+
+const happyBirthdayPet = (breed, age) => {
+  if (breed === 'snake') {
+    return 'Hiss hiss!';
+  } else if (breed === 'cat') {
+    if (age < 5) {
+      return 'Mew mew!';
+    } else if (age >= 5) {
+      return 'Meow meow!'; 
+    }
+  } else if (breed === 'dog') {
+    if (age < 5) {
+      return 'Arf Arf!';
+    } else if (age >= 5 && age < 10) {
+      return 'Woof woof!'; 
+    } else if (age >= 10) {
+      return 'Boof!';
+    };
+  } else return 'Happy birthday!';
 };
+
+console.log(happyBirthdayPet('snake', 3)); // "Hiss hiss!"
+console.log(happyBirthdayPet('cat', 2)); // "Mew mew!"
+console.log(happyBirthdayPet('dog', 12)); // "Boof!"
 
 const funTypes = () => {
 };
